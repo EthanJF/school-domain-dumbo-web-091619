b71ea10fd66a438d4ad0072f4930e9c86d301f67
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-18 10:24:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,11 @@
 # code here!
 class School
-  attr_reader :add_student
+  attr_reader :add_student, :roster
   
   def initialize
-    
-  
-  roster = {}
-  
+    @roster = {}
+  end
+
   def add_student(name, grade)
     roster[:name] = :grade
   end
